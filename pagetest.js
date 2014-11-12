2e224609390cbb49d0ae141b0138a5dd4c9be5fd
Jasper Lu <repsajrules@gmail.com>
2014-11-12 02:29:58 -0500

    moved keys

--- pagetest.js
+++ pagetest.js
@@ -24,9 +24,9 @@ if (Meteor.isClient) {
         'mousedown .role' : function(e) {
             toggleHidden($(e.target));
         },
-        'mouseup .role' : function(e) {
-            toggleHidden($(e.target));
-        }
+    'mouseup .role' : function(e) {
+        toggleHidden($(e.target));
+    }
     });
 
     Template.game.matches = function() {
@@ -34,6 +34,66 @@ if (Meteor.isClient) {
             return Session.get("matches");
         return null;
     }
+
+    scopes = 'https://www.googleapis.com/auth/analytics.readonly';
+
+    var parseDate = function(d) {
+        s = d.toString();
+        date = new Date(s.substring(0,4), s.substring(4,6) - 1, s.substring(6));
+        return date;
+    }
+
+    var drawChart = function(data) {
+        var ctx = $('#lineGraph').get(0).getContext("2d");
+        var newChart = new Chart(ctx);
+        newChart.Line(data);
+    }
+
+
+    var apiCallResultHandler = function(r) {
+                        console.log(r);
+                        var labels = [];
+                        var data = [];
+                        _.each(r.rows, function(row) {
+                            labels.push(parseDate(row[0]).toDateString());
+                            data.push(row[1]);
+                        });
+                        console.log(labels);
+                        console.log(data);
+                        var lineData = {
+                            labels: labels,
+                            datasets: [{
+                                fillColor : "rgba(220,220,220,0.5)",
+                                strokeColor : "rgba(220,220,220,1)",
+                                pointColor : "rgba(220,220,220,1)",
+                                pointStrokeColor : "#fff",
+                                data: data 
+                            }]
+                        }
+                        drawChart(lineData);
+
+    }
+
+    Template.analyticsBody.events({
+        'click #al': function() {
+            console.log("clicked");
+            gapi.client.setApiKey(apiKey);
+            gapi.auth.authorize({client_id: clientId, scope: scopes}, function() {
+                gapi.client.load("analytics", "v3", function() {
+                    console.log("loaded");
+                    var apiQuery = gapi.client.analytics.data.ga.get({
+                        'ids': "ga:88189745",
+                        'start-date': '7daysAgo',
+                        'end-date':'yesterday',
+                        'metrics': 'ga:sessions',
+                        'dimensions' : 'ga:date',
+                        'max-results': 25
+                    });
+                    apiQuery.execute(apiCallResultHandler);
+                });
+            });
+        }
+    });
 }
 
 if (Meteor.isServer) {
@@ -43,8 +103,8 @@ if (Meteor.isServer) {
         // code to run on server at startup
         connection = mysql.createConnection({
             host: 'jasperlu.com',
-                   user: 'test',
-                   password: 'testing123',
+                   user: mysqlUser.user,
+                   mysqluser.password,
                    database: 'page'
         });
         connection.connect(function(e) {
