4b8ca3bec6f5091615d023be4f7486852a4bac4e
Jasper Lu <repsajrules@gmail.com>
2014-11-12 10:48:23 -0500

    added vtp charts w/o ga

--- client/latest_customers.js
+++ client/latest_customers.js
@@ -1,19 +1,19 @@
 
 Template.latestCustomers.rendered = function() {
-    Meteor.call("query", "select s.id, s.first_payment, s.next_payment, s.uid, s.pageid, u.firstname, u.lastname, u.email from stripe_subscriptions as s, users as u where s.canceled = 0 and u.uid=s.uid limit 10;", function(e, r) {
+    Meteor.call("query", "select s.id, s.first_payment, s.next_payment, s.uid, s.pageid, u.firstname, u.lastname, u.email from stripe_subscriptions as s, users as u where s.canceled = 0 and u.uid=s.uid order by first_payment desc limit 10;", function(e, r) {
         if(e) {
             //error check
             console.log(e);
             return -1;
         }
         console.log(r);
-        Session.set("latest_customers", r);
+        Session.set("latest_customers_month", r);
     });
 }
 
 Template.latestCustomers.subscriptions = function() {
     console.log(Session.get("latest_customers")); 
-    return Session.get("latest_customers");
+    return Session.get("latest_customers_month");
 }
 
 Template.lcRow.firstPayment = function() {
--- client/vtp.js
+++ client/vtp.js
@@ -1,22 +1,55 @@
 Template.monthlyVTP.rendered = function() {
     var ctx = document.getElementById("monthlyVTP").getContext("2d");
     var data = [{
-        value: 0,
+        value: 1,
         color: "#4eb972",
         label: "Pagevamp Visits"
     },{
-        value: 0,
+        value: 1,
         color: "#e67e22",
         label: "Trials"
     },{
-        value: 0,
+        value: 1,
         color:"#8e44ad",
         label: "Customers"
     }]
     month = new Chart(ctx).Doughnut(data, {});
-    Meteor.call("query", "SELECT count(id) FROM stripe_subscriptions WHERE from_unixtime(first_payment)  BETWEEN (CURRENT_DATE() - INTERVAL 1 MONTH) AND CURRENT_DATE() and canceled = 0;", function(e, r) {
+    $("#monthlyVTPLegend").html(month.generateLegend());
+    Meteor.call("query", "select (select count(id) from fbpages where published_by = 0 and from_unixtime(created_on) BETWEEN (CURRENT_DATE() - INTERVAL 1 MONTH) AND CURRENT_DATE()) as trials_monthly, (SELECT count(id) FROM stripe_subscriptions WHERE from_unixtime(first_payment)  BETWEEN (CURRENT_DATE() - INTERVAL 1 MONTH) AND CURRENT_DATE() and canceled = 0) as customers_monthly;", function(e, r) {
         console.log(r);
-        month.segments[2].value = r[0]['count(id)'];
+        console.log(r[0].trials_monthly);
+        month.segments[1].value = r[0].trials_monthly;
+        month.segments[2].value = r[0].customers_monthly;
+        /*
+        data[0].value = 100;
+        data[1].value = r[0].trials_monthly;
+        data[2].value = r[0].customers_monthly; 
+        */
         month.update();
+    });
+}
+
+Template.weeklyVTP.rendered = function() {
+    var ctx = document.getElementById("weeklyVTP").getContext("2d");
+    var data = [{
+        value: 1,
+        color: "#4eb972",
+        label: "Pagevamp Visits"
+    },{
+        value: 1,
+        color: "#e67e22",
+        label: "Trials"
+    },{
+        value: 1,
+        color:"#8e44ad",
+        label: "Customers"
+    }]
+    week = new Chart(ctx).Doughnut(data, {});
+    $("#weeklyVTPLegend").html(week.generateLegend());
+    Meteor.call("query", "select (select count(id) from fbpages where published_by = 0 and from_unixtime(created_on) BETWEEN (CURRENT_DATE() - INTERVAL 1 WEEK) AND CURRENT_DATE()) as trials_weekly, (SELECT count(id) FROM stripe_subscriptions WHERE from_unixtime(first_payment)  BETWEEN (CURRENT_DATE() - INTERVAL 1 WEEK) AND CURRENT_DATE() and canceled = 0) as customers_weekly;", function(e, r) {
+        console.log(r);
+        week.segments[1].value = r[0].trials_weekly;
+        week.segments[2].value = r[0].customers_weekly;
+        week.update();
     });
 }
